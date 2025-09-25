18a8353a08920e691cdc4df4cca61a4e09170832
spdtttt <spdt101mos@gmail.com>
2025-09-25 22:57:28 +0700

    Add I Love Baifern

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,10 +1,13 @@
-import { h1 } from 'motion/react-client';
-import '../App.css'
+import { h1 } from "motion/react-client";
+import "../App.css";
 
 function Booking() {
-    return (
-        <h1>kuy</h1>
-    )
+  return (
+    <>
+      <h1>kuy</h1>
+      <h1>I love Baifern makmak</h1>
+    </>
+  );
 }
 
 export default Booking;
